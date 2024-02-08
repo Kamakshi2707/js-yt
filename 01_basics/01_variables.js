0b5a0112273995625c5ea65bfa68273df5db2a26
Kamakshi2707 <136990129+Kamakshi2707@users.noreply.github.com>
2024-02-08 12:32:16 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "kamakshi@gmail.com"
+var accountPassword ="12345"
+accountCity = "indore"
+
+
+//accountId = 2
+
+accountEmail = "ram@gmail.com"
+accountPassword = "121212"
+accountCity="mumbai"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
