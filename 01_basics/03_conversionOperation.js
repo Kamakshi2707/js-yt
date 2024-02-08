cf6d811235ba96c30ed5c7b5d2e7712228e49e46
Kamakshi2707 <136990129+Kamakshi2707@users.noreply.github.com>
2024-02-08 13:13:20 +0000

    datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "hjh33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
+// "33"=> 33
+// "33abc"=>NaN
+// true=>1
+
+
+let isloggedin="kamakshi"
+
+let boolisloggedin = Boolean(isloggedin)
+console.log(boolisloggedin);
+
+//1=>true; 0=>false
+//""=>false
+// "kamakshi"=>true
+
+let someNumber = 33
+let stringNumber =String(someNumber);
+console.log(typeof (stringNumber));
